14c3bfb39c69b220da81a0cd59c033dd6435c9a5
Onur Aykutalp <onuraykutalp@gmail.com>
2025-12-05 04:10:24 +0300

    son update

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
   images: {
-    domains: ["images.unsplash.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.elitedinnercruise.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "your-other-image-cdn.com",
+      },
+    ],
   },
 };
 
